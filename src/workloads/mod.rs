725e2d897d08ba3f35983e1cb9f6db4c8602854b
Pranav M <pranavmofficial2005@gmail.com>
2026-05-23 21:46:14 +0530

    refactor: export WorkingSetSweep and WorkingSetResult types

--- src/workloads/mod.rs
+++ src/workloads/mod.rs
@@ -1,7 +1,9 @@
 pub mod dataset_scan;
 pub mod vector_add;
 pub mod pointer_chase;
+pub mod working_set_sweep;
 
 pub use dataset_scan::{DatasetScan, ScanComparison};
 pub use vector_add::{VectorAdd, VectorAddComparison};
 pub use pointer_chase::{PointerChase, PointerChaseComparison};
+pub use working_set_sweep::{WorkingSetSweep, WorkingSetResult};
